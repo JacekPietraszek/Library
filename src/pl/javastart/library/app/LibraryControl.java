1860cc4d659bbd4a803d34ce7a4416bb2d3b85a6
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-09-05 10:13:26 +0200

    Improves the selection of the options in the programme

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -6,23 +6,18 @@ import pl.javastart.library.model.Library;
 import pl.javastart.library.model.Magazine;
 
 public class LibraryControl {
-    // stałe do kontrolowania programu
-    private static final int EXIT = 0;
-    private static final int ADD_BOOK = 1;
-    private static final int ADD_MAGAZINE = 2;
-    private static final int PRINT_BOOKS = 3;
-    private static final int PRINT_MAGAZINES = 4;
+
 
     private DataReader dataReader = new DataReader();
 
     private Library library = new Library();
 
     public void controlLoop() {
-        int option;
+        Option option;
 
         do {
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
             switch (option) {
                 case ADD_BOOK:
                     addBook();
@@ -42,7 +37,7 @@ public class LibraryControl {
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
             }
-        } while (option != EXIT);
+        } while (option != Option.EXIT);
     }
 
 
@@ -72,10 +67,8 @@ public class LibraryControl {
 
     private void printOptions() {
         System.out.println("Wybierz opcję: ");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        for (Option value : Option.values()) {
+            System.out.println(value);
+        }
     }
 }
